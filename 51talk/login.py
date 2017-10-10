de0fa83c5cb1747b44c64256b7522e722ac0762c
孙业军 <yejun.sun@lavector.com>
2017-10-10 18:33:27 +0800

    modify login

--- 51talk/login.py
+++ 51talk/login.py
@@ -1,7 +1,13 @@
 # -*- coding: utf-8 -*-
+# from __future__ import unicode_literals, print_function  # python2
 import requests
 import json
 import logging
+import hashlib
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_v1_5
+import base64
+
 
 class Login:
 
@@ -9,7 +15,8 @@ class Login:
         self.public_key = ""
         self.login_la = ""
         self.username = username
-        self.password = password
+        self.password2 = password
+        self.password = ""
 
     def get_public_key(self):
         url = "http://login.51talk.com/sso/publickey?client=1"
@@ -18,6 +25,7 @@ class Login:
             if r.status_code == 200:
                 pub_key = json.loads(r.text.replace("try{(", "").replace(")}catch(e){};", ""))
                 self.public_key = pub_key["res"]["rsa_pub"]
+                self.password = self.encrypt(self.password2)
                 return self.public_key
             else:
                 logging.error('Could not get the public key')
@@ -28,7 +36,8 @@ class Login:
                 logging.exception('Could not get the public key', exception)
             return None
     def pre_login(self):
-        url ="http://login.51talk.com/sso/prelogin?client=1&username=" + self.username + "&password2=" + self.password
+        url ="http://login.51talk.com/sso/prelogin?client=1&username=" + self.username + "&password2=" + self.password2 + "&password=" + self.password
+        print(url)
         try:
             r = requests.get(url)
             if r.status_code == 200:
@@ -43,9 +52,38 @@ class Login:
             if is_debug_run():
                 logging.exception('pre login error', exception)
             return None
+    def login(self):
+        data = {
+            "from_url":"",
+            "client" : 1,
+            "autologin" : 1,
+            "username" : self.username,
+            "password" : self.password,
+            "la" : self.login_la
+        }
+        url = "http://login.51talk.com/ajax/login"
+        try:
+            headers = {'Content-type': 'application/x-www-form-urlencoded; charset=UTF-8;User-Agent:Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'}
+            r = requests.post(url, data=data, headers=headers)
+            if r.status_code == 200:
+                print(r.text)
+        except requests.exceptions.HTTPError as exception:
+            logging.error("login error",exception)
+            if is_debug_run():
+                logging.exception("login error", exception)
+            return None
+
+    def encrypt(self, msg):
+        rsa = RSA.importKey(self.public_key)
+        cipher = PKCS1_v1_5.new(rsa)
+        ciphertext = cipher.encrypt(msg.encode('utf8'))
+        return base64.b64encode(ciphertext).decode('ascii')
+
 
 if __name__ == "__main__":
-    login = Login("186", "xxx")
-    print(login.pre_login())
-    # print(login.get_public_key())
-
+    login = Login("186xxxxxxx", "abc")
+    # print(login.pre_login())
+    login.get_public_key()
+    login.pre_login()
+    login.login()
+#
--- assets/51talkLogin.js
+++ assets/51talkLogin.js
@@ -1,123 +1,182 @@
-/**
- * Created by chenggang on 15/10/27.
- */
-var ssoController = {
-    preLogin:function(){
-        $.ajax({
-            url: 'http://login.51talk.com/sso/prelogin',
-            dataType: 'jsonp',
-            jsonpCallback: 'preLoginCallBack',
-            data: $('#LoginForm').serialize(),
-            type: 'get',
-            success: function(ret) {
-                $('#login_la').val(ret.res.la);
-                $("#LoginForm").submit();
-            }
-        });
-    },
-    feedBack: function(dataObj){
-        var data = eval(dataObj);
-        if(data.code == 10000){
-            if(data.from_url){
-                window.location.href= data.from_url;
-            }else{
-                window.location.reload();
-            }
-        }else{
-            alert(data.msg);
-//      $("#login_submit").removeAttr("disabled");
-        }
-    },
-    getPublicKey:function(){
-        var client_id = $('input[name=client]').val();
-        $.ajax({
-            url: 'http://login.51talk.com/sso/publickey',
-            dataType: 'jsonp',
-            jsonpCallback: 'pubkeyCallBack',
-            data: {'client':client_id},
-            type: 'get',
-            success: function(ret) {
-                $("#public_key").val(ret.res.rsa_pub);
-            }
-        });
+
+define("login/user_login", [], function(require, exports, module) {
+    function showError(a) {
+        a.parent().addClass("input-error"),
+        a.parent().siblings(".error").css("display", "block")
     }
-};
-
-
-$(function(){
-    var sso_switch = $("input[name=sso_switch]").val();
-    var ori_login_url = $("input[name=ori_login_url]").val();
-    $("body").append("<iframe id='ssoLoginFrame' name='ssoLoginFrame' width='0' height='0' style='display:none;'></iframe>");
-    if(sso_switch == 1){
-        $("#LoginForm").attr("target", "ssoLoginFrame");
-        $("#LoginForm").attr("action", "http://login.51talk.com/sso/login");
-        $("#LoginForm").append("<input type=\"hidden\" name=\"public_key\" id=\"public_key\" /><input type=\"hidden\" name=\"la\" id=\"login_la\" /><input type=\"hidden\" name=\"group\" id=\"group\" value=\"4\" />");
-        $("#password").removeAttr("name");
-        $("#LoginForm").append("<input type=\"hidden\" name=\"password\" id=\"password_rsa\" />");
-    }else{
-        $("#LoginForm").attr("action", ori_login_url);
+    function showOff(a, r) {
+        console.log(a);
+        var e = $("#" + r);
+        "" == e.val() ? (e.parent().siblings(".error").hide(),
+        e.parent().removeClass("input-error"),
+        e.parent().removeClass("correct")) : 1 == a ? e.parent().addClass("correct") : (e.parent().siblings(".error").show(),
+        e.parent().removeClass("correct"))
     }
-
-    // prelogin
-    $('#username').blur(function() {
+    function startLogin() {
+        $("#LoginForm");
         $.ajax({
-            url: 'http://login.51talk.com/sso/prelogin',
-            dataType: 'jsonp',
-            jsonpCallback: 'preLoginCallBack',
-            data: $('#LoginForm').serialize(),
-            type: 'get',
-            success: function(ret) {
-                $('#login_la').val(ret.res.la);
-            }
-        });
-
-        return false;
-    }).focus(function(){
-        ssoController.getPublicKey();
-    });
-
-    $('#password').focus(function(){
-        ssoController.getPublicKey();
-    });
-
-    //提交登录
-    $("#login_submit").click(function(){
-        if(sso_switch != 1){
-            $("#LoginForm").submit();
-            return false;
-        }
-        var password = $("#password").val(),
-            username = $("#username").val(),
-            login_la = $('#login_la').val(),
-            public_key = $('#public_key').val();
-        if( username == '' || username == '已验证手机号/邮箱' ){
-            alert('请输入用户名');
-            return false;
-        }
-        if( password == '' || password == '请输入您的密码' ){
-            alert('请输入密码');
-            return false;
-        }
-
-        if(public_key == ''){
-            alert('您的用户名或密码错误');
-            ssoController.getPublicKey();
-            return false;
-        }
-
-        var encrypt = new JSEncrypt();
-        encrypt.setPublicKey(public_key);
-        var encrypted = encrypt.encrypt(password);
-        $("#password_rsa").val(encrypted);
-
-                if(!encrypted || encrypted=="false"){
-                    $("#password_rsa").val(hex_md5(password));
+            url: $("#LoginForm").attr("action"),
+            type: "post",
+            data: {
+                username: $("#username").val(),
+                password: $("#password").val(),
+                la: $("#login_la").val(),
+                client: $("#client").val(),
+                lang: $("#lang").val(),
+                from_url: $("#from_url").val(),
+                autologin: $("#autologin").val()
+            },
+            success: function(a) {
+                if (1e4 == a.code)
+                    a.res.from_url ? window.location.href = a.res.from_url : window.location.reload();
+                else {
+                    $("#password2").parent().removeClass("correct");
+                    var r = $("#password2").parent().siblings(".error").find("span");
+                    r.text(a.message),
+                    showError($("#password2")),
+                    $("#password2").attr("name", "password2")
                 }
-        if(login_la == ''){
-            ssoController.preLogin();
-        }else{
-            $("#LoginForm").submit();
-        }
-
+            }
+        })
+    }
+    function validateUsername() {
+        var a = /^(([a-z0-9\+_\-]+)(\.[a-z0-9\+_\-]+)*@([a-z0-9\-]+\.)+[a-z]{2,6})|(1[0-9]{10})$/i
+          , r = "username"
+          , e = $("#" + r).val()
+          , o = $("#" + r).parent().siblings(".error").find("span");
+        return e ? a.test(e) ? ($.ajax({
+            url: "http://login.51talk.com/ajax/mobile/check",
+            data: {
+                mobile: e,
+                client: client
+            },
+            type: "post",
+            async: !1,
+            dataType: "json",
+            success: function(a) {
+                return (a.code = 1e4) ? void (flag = !0) : (o.text(a.message),
+                showError($("#" + r)),
+                void (flag = !1))
+            },
+            error: function() {
+                alert("\u7f51\u7edc\u5f02\u5e38"),
+                flag = !1
+            }
+        }),
+        flag) : (o.text("\u7528\u6237\u540d\u683c\u5f0f\u9519\u8bef"),
+        showError($("#" + r)),
+        !1) : (o.text("\u8bf7\u8f93\u5165\u624b\u673a\u53f7\u7801\u6216\u90ae\u7bb1"),
+        showError($("#" + r)),
+        !1)
+    }
+    function validatePassword2() {
+        var a = /^[\w\+\!\@\#\$\%\^\&\*\(\)]{6,20}$/
+          , r = "password2"
+          , e = $("#" + r).val()
+          , o = $("#" + r).parent().siblings(".error").find("span");
+        return e ? a.test(e) ? !0 : (o.text("\u5bc6\u7801\u957f\u5ea6\u53ea\u80fd\u662f6-20\u4f4d\u5b57\u7b26"),
+        showError($("#" + r)),
+        !1) : (o.text("\u8bf7\u8f93\u5165\u5bc6\u7801"),
+        showError($("#" + r)),
+        !1)
+    }
+    document.domain = "51talk.com",
+    $(".s-input,.m-code-input").focus(function() {
+        $(this).parent().siblings(".error").hide(),
+        $(this).parent().removeClass("input-error")
+    }),
+    $(".s-input").blur(function() {
+        var id = $(this).attr("id")
+          , fullName = "var value = validate" + id.substring(0, 1).toUpperCase() + id.substring(1) + "()";
+        eval(fullName),
+        showOff(value, id)
     });
+    var flag = !0
+      , client = $("#client").val();
+    $("#LoginForm").on("submit", function(a) {
+        var r = $(this);
+        if (r.attr("submited"))
+            return !0;
+        var e = $("#login_la").val()
+          , o = $("#public_key").val()
+          , t = $("#password2").val();
+        o || ssoController.getPublicKey();
+        var s = new JSEncrypt;
+        s.setPublicKey(o);
+        var n = s.encrypt(t);
+        return $("#password").val(n),
+        n && "false" != n || $("#password").val(hex_md5(t)),
+        e ? ($("#password2").removeAttr("name"),
+        startLogin()) : ssoController.preLogin(),
+        !1
+    }),
+    $("#login").click(function() {
+        return validateUsername() && validatePassword2() ? void $("#LoginForm").submit() : flag = !1
+    }),
+    window.ssoController = {
+        preLogin: function() {
+            $.ajax({
+                url: "http://login.51talk.com/sso/prelogin",
+                dataType: "jsonp",
+                jsonpCallback: "preLoginCallBack",
+                data: $("#LoginForm").serialize(),
+                type: "get",
+                success: function(a) {
+                    $("#login_la").val(a.res.la),
+                    $("#password2").removeAttr("name"),
+                    startLogin()
+                }
+            })
+        },
+        feedBack: function(dataObj) {
+            var data = eval(dataObj);
+            1e4 == data.code ? data.from_url ? top.location.href = data.from_url : top.location.reload() : (alert(data.msg),
+            $("#password2").attr("name", "password2"),
+            $("#LoginForm").removeAttr("submited").find(".jsSubmit").val("\u767b\u5f55"))
+        },
+        getPublicKey: function() {
+            var a = $("input[name=client]").val() || 1;
+            $("#public_key").val() || $.ajax({
+                url: "http://login.51talk.com/sso/publickey",
+                dataType: "jsonp",
+                jsonpCallback: "pubkeyCallBack",
+                data: {
+                    client: a
+                },
+                type: "get",
+                success: function(a) {
+                    $("#public_key").val(a.res.rsa_pub)
+                }
+            })
+        }
+    },
+    function() {
+        var a = $("#LoginForm");
+        $("body").append("<iframe id='ssoLoginFrame' name='ssoLoginFrame' width='0' height='0' style='display:none;'></iframe>"),
+        ssoController.getPublicKey(),
+        $("#username").blur(function() {
+            return $.ajax({
+                url: "http://login.51talk.com/sso/prelogin",
+                dataType: "jsonp",
+                jsonpCallback: "preLoginCallBack",
+                data: a.serialize(),
+                type: "get",
+                success: function(a) {
+                    $("#login_la").val(a.res.la)
+                }
+            }),
+            !1
+        }).focus(function() {
+            ssoController.getPublicKey()
+        }),
+        $("#password").focus(function() {
+            ssoController.getPublicKey()
+        })
+    }(),
+    $("#checkbox").click(function() {
+        $(this).toggleClass("checked"),
+        $(this).hasClass("checked") ? ($(".inputChed").val("1"),
+        $(".inputChed").parent().siblings(".error").hide()) : $(".inputChed").val("0")
+    })
 });
+
